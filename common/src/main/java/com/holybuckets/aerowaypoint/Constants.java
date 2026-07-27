b28ad0c6e4f1c92aa9adda44409db4be881a3f2b
jhw2167 <jhw2167@columbia.edu>
2026-07-27 09:51:39 -0500

    version updates

--- common/src/main/java/com/holybuckets/aerowaypoint/Constants.java
+++ common/src/main/java/com/holybuckets/aerowaypoint/Constants.java
@@ -15,9 +15,9 @@ public class Constants {
 
 	public static final List<BlockPos> LOCAL_POINTS = new ArrayList<>();
 	static {
-		for (int x = -1; x <= 1; x++) {
-			for (int y = -1; y <= 1; y++) {
-				for (int z = -1; z <= 1; z++) {
+		for (int x = -3; x <= 3; x++) {
+			for (int y = -2; y <= 2; y++) {
+				for (int z = -3; z <= 3; z++) {
 					LOCAL_POINTS.add(new BlockPos(x, y, z));
 				}
 			}
